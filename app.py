88253f60b397e49882f4b1bb96516ba160891966
zxqlxy <liuxinyun6@yahoo.com>
2020-01-24 23:15:59 -0600

    add someting

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import pandas as pd
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+from sklearn import decomposition
+from sklearn.preprocessing import StandardScaler
+from sklearn import decomposition
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.model_selection import train_test_split
+import random
+
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
@@ -11,6 +20,14 @@ if uploaded_file is not None:
 
 st.write(data.describe())
 
+def checktype(df : pd.DataFrame):
+
+	values = df.dtypes
+	typeDic = values.to_dict()
+	numDic = dict(filter(lambda elem: elem[1] == "float64" or elem[1] == "int64", typeDic.items()))
+	return list(numDic.keys())
+dhead = data.head(10)
+columns = checktype(dhead)
 # for col in data.columns: 
 #     print(col) 
 #     st.button(col)
@@ -18,8 +35,9 @@ st.write(data.describe())
 options = st.multiselect(
 	     'Choose two parameter to plot',
          # ('Yellow', 'Red')
-	     data.columns)
+	     columns)
 # st.write('You selected:', options)
+
 
 def plot2(x,y):
 	plt.plot(x,y)
@@ -50,29 +68,43 @@ elif len(options) == 3:
 	plot3(data[options[0]], data[options[1]], data[options[2]])
 
 
+num_data = data[columns]
 def plot_PCA(option='standard'):
     pca = decomposition.PCA()
-    processed = process_all(0, 2000)
     if option == 'standard':
         standard = StandardScaler()
-        standard.fit(processed)  # Standardize the data
+        standard.fit(num_data)  # Standardize the data
         stand = standard
-        processed = standard.transform(processed)
-        pca.fit(processed)
-        newdata = pca.transform(processed)
+        processed = standard.transform(num_data)
+        pca.fit(num_data)
+        newdata = pca.transform(num_data)
     elif option == 'normal':
-        pca.fit(processed)
-        newdata = pca.transform(processed)
+        pca.fit(num_data)
+        newdata = pca.transform(num_data)
     # print(pca.get_covariance())
     plt.matshow(pca.components_, cmap='viridis')
     # plt.yticks([0,1,2],['1st Comp','2nd Comp','3rd Comp'],fontsize=10)
     plt.colorbar()
-    plt.xticks(range(6), ['time', 'bins', 'element', 'Amplitude', 'Doppler', 'FWHM'], rotation=65, ha='left')
+    plt.xticks(range(len(columns)), columns, rotation=65, ha='left')
     plt.tight_layout()
     st.pyplot()
     plt.clf()
 
-	
+plot_PCA();
+
+def plot_correlation():
+	f = plt.figure(figsize=(19, 15))
+	plt.matshow(data.corr(), fignum=f.number)
+	plt.xticks(range(len(columns)), columns, fontsize=14, rotation=45)
+	plt.yticks(range(len(columns)), columns, fontsize=14)
+	cb = plt.colorbar()
+	cb.ax.tick_params(labelsize=14)
+	plt.title('Correlation Matrix', fontsize=16)
+	st.pyplot()
+	plt.clf()
+
+
+plot_correlation()
 # progress_bar = st.progress(0)
 # status_text = st.empty()
 # chart = st.line_chart(np.random.randn(10, 2))
